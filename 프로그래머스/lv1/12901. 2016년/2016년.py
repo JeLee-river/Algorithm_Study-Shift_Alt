ff7c6f514ff64736d16d7264db47a98bd37153b1
JeLee-river <106021313+JeLee-river@users.noreply.github.com>
2023-09-08 23:55:35 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -0,0 +1,11 @@
+from functools import reduce
+def solution(a, b):
+    DAYS = {0:'THU', 1: 'FRI', 2:'SAT', 3:'SUN', 4:'MON', 5:'TUE', 6:'WED'}
+    dates = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    targetMonths = dates[0:a-1]
+    totalDays = reduce(lambda acc, cur: acc+cur,targetMonths, b);
+    return DAYS[totalDays%7]
+    
+    
+    answer = ''
+    return answer
