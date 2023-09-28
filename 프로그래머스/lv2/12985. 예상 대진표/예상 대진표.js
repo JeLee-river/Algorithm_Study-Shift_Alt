dfb657d1f53b55b91191a95959ef8e8249197ea4
JeLee-river <106021313+JeLee-river@users.noreply.github.com>
2023-09-28 17:54:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,12 +1,12 @@
-function solution(n,a,b) {
-    let forwardAttendee = Math.min(a,b);
-    let backwardAttendee = Math.max(a,b);
-    let gameCounter = 1;
-    
-    while(forwardAttendee + 1 !== backwardAttendee || forwardAttendee%2 === 0) {
-        forwardAttendee = Math.ceil(forwardAttendee/2);
-        backwardAttendee = Math.ceil(backwardAttendee/2);
-        ++gameCounter;
-    }
-    return gameCounter;
+function solution(n, a, b) {
+  let forwardAttendee = Math.min(a, b);
+  let backwardAttendee = Math.max(a, b);
+  let gameCounter = 0;
+
+  while (forwardAttendee !== backwardAttendee) {
+    forwardAttendee = Math.ceil(forwardAttendee / 2);
+    backwardAttendee = Math.ceil(backwardAttendee / 2);
+    ++gameCounter;
+  }
+  return gameCounter;
 }
