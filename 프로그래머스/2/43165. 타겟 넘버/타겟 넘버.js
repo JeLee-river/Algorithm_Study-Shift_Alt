801d650532b220026b20876f0a0c66b45b74f259
JeLee-river <106021313+JeLee-river@users.noreply.github.com>
2024-03-01 21:44:12 +0900

    [level 2] Title: 타겟 넘버, Time: 0.48 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,22 @@
+function solution(numbers, target) {
+    const sumNumber = numbers.reduce((accSum, num) => accSum + num);
+    let answer = 0;
+    
+    const calculator = (currentValue, currentIndex) => {
+        if(currentValue === target){
+            answer += 1;
+        } else {
+            if(currentValue < target || currentIndex > numbers.length-1){
+                return;
+            }
+            
+            for(let i = currentIndex+1; i <= numbers.length-1; i+= 1){
+                calculator(currentValue - 2 * numbers[i], i);
+            }
+        }
+    }
+    
+    calculator(sumNumber, -1);
+    
+    return answer;
+}
