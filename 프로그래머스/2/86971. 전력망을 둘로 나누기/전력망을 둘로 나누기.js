cc180f946965621fe03c6f9a740ed3ffb0bc005c
JeLee-river <106021313+JeLee-river@users.noreply.github.com>
2024-04-23 16:40:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.30 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,44 +1,25 @@
 function solution(n, wires) {
-    const linkedTable = new Map();
-    wires.forEach((pair) => {
-        pair.forEach((wire, index) => {
-            const linkedArray = linkedTable.get(wire) ?? [];
-            linkedArray.push(pair[1-index]);
-            linkedTable.set(wire, linkedArray);  
-        });
-    });
-    
-    const answer = [];
-    
-    for (let i = 1; i <= n; i+=1){
-        const linkedWires = linkedTable.get(i);
-        
-        for (let wireIndex = 0; wireIndex <= linkedWires.length-1; wireIndex+=1){
-            const tree = new Map(linkedTable);
-            const disconnected1 = linkedWires.filter((wire) => wire !== linkedWires[wireIndex]);
-            
-            const disconnectedWire = linkedTable.get(linkedWires[wireIndex]);
-            const disConnected2 = disconnectedWire.filter((wire) => wire !== i);
-
-            tree.set(i, disconnected1);
-            tree.set(linkedWires[wireIndex], disConnected2);
-            
-            const visited = new Set();
-            const queue = [1];
-
-            while(queue.length > 0){
-                const currentWire = queue.shift();
-                if(!visited.has(currentWire)){
-                    visited.add(currentWire);
-                    const linkedWires = tree.get(currentWire);
-                    linkedWires.forEach((wire) => {
-                        queue.push(wire);
-                    });
-                }
-            }
-            answer.push(visited.size);
+    const g=Array.from({length:n},()=>[]);
+    for(const e of wires){
+        g[e[0]-1].push(e[1]-1);
+        g[e[1]-1].push(e[0]-1);
+    }
+    const p=new Array(n).fill(-1);
+    const q=[0];
+    for(let i=0;i<q.length;++i){
+        const u=q[i];
+        for(const v of g[u])if(v!=p[u]){
+            p[v]=u;
+            q.push(v);
         }
     }
-    
-    return Math.min(...answer.map((wires) => Math.abs(n - 2 * wires)));
+    let ans=n;
+    const dp=new Array(n).fill(1);
+    for(let i=q.length;--i>0;){
+        const v=q[i];
+        dp[p[v]]+=dp[v];
+        let a=Math.abs(n-2*dp[v]);
+        if(ans>a)ans=a;
+    }
+    return ans;
 }
