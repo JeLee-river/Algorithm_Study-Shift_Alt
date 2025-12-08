d76c609a1e99b9675e46c9209b701f921c3fc26b
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2025-12-09 04:57:50 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let power = 0;
+    while (n > 0) {
+        if(n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = (n - 1) / 2;
+            power += 1;
+        }    
+    }
+    
+    
+    return power;
+}
+
