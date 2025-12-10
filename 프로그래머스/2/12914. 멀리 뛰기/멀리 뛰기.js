531c3b20021ed62f1e264c0c18298cc48ee5efef
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2025-12-11 04:41:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    const totalAnswer = Array.from({length: n}, () => 0);
-    totalAnswer[0] = 1%1234567;
-    totalAnswer[1] = 2%1234567;
-    for (let i = 2; i<= totalAnswer.length-1; i++){
-        totalAnswer[i] = (totalAnswer[i-1] + totalAnswer[i-2])%1234567;
+    const dp = Array.from({length: n + 1}, () => 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= dp.length - 1; i += 1) {
+        dp[i] = dp[i - 2] % 1234567 + dp[i - 1] % 1234567;
     }
-    return totalAnswer[n-1];
+    
+    return dp[n] % 1234567;
 }
