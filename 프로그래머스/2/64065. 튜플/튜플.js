816e1c53edf239804f68c5e3ccdc5583c35e2293
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-01-09 22:34:08 +0900

    [level 2] Title: 튜플, Time: 44.90 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,9 +1,15 @@
 function solution(s) {
-    const removedBracket = s.slice(2,-2);
-    const tupleArr = removedBracket.split('},{');
-    tupleArr.sort((a,b) => a.length - b.length);
-    const splitedTuple = tupleArr.join();
-    const splitedTupleArr = splitedTuple.split(',');
-    const removedDuplication = new Set(splitedTupleArr);
-    return Array.from(removedDuplication).map((num) => Number(num))
+    const sArr = s.split('},{');
+    // sArr[0] = sArr[0][2];
+    // sArr[sArr.length-1] = sArr[sArr.length-1].replaceAll('}','');
+    const tuppleArray = sArr.map((str) => {
+        const newStr = str.replace(/[{}]/g, '');
+        return newStr.split(',');
+    });
+    
+    tuppleArray.sort((prev, next) => prev.length - next.length);
+    const newTuppleArray = tuppleArray.flat();
+    const tupple = new Set();
+    newTuppleArray.forEach((el) => tupple.add(el));
+    return Array.from(tupple).map(Number);
 }
