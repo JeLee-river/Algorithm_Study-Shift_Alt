67c7dcec494d7230b4ef2a9d156b953c77620f0e
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-01-15 19:48:07 +0900

    [level 2] Title: 타겟 넘버, Time: 14.51 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,22 +1,18 @@
 function solution(numbers, target) {
-    const sumNumber = numbers.reduce((accSum, num) => accSum + num);
     let answer = 0;
-    
-    const calculator = (currentValue, currentIndex) => {
-        if(currentValue === target){
-            answer += 1;
-        } else {
-            if(currentValue < target){
-                return;
+    const caculator = (current, index) => {
+        if(index === numbers.length) {
+            if(current === target) {
+                answer +=1;   
             }
-            
-            for(let i = currentIndex+1; i <= numbers.length-1; i+= 1){
-                calculator(currentValue - 2 * numbers[i], i);
-            }
+            return;
         }
+        
+        const currentNumber = numbers[index];
+        caculator(current + currentNumber, index + 1);
+        caculator(current - currentNumber, index + 1);
     }
     
-    calculator(sumNumber, -1);
-    
+    caculator(0, 0);
     return answer;
 }
