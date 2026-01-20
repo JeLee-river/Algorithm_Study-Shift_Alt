3afdb0099a702d9e145f17870b7502d06f730149
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-01-20 20:58:35 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 37.63 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,28 +1,15 @@
 function solution(n, k) {
-    const target = n.toString(k);
-    const splitedTarget = target.split('0');
-    const filteredTarget = splitedTarget.filter((number) => number !== '');
-    let answer = 0;
-    
-    filteredTarget.forEach((number) => {
-        if(isPrime(Number(number))) {
-            answer += 1;
+    const isPrimeNumber = (numString) => {
+        const num = Number(numString);
+        if (num === 1) return false;
+        for (let i = 2; i<= Math.sqrt(num); i += 1) {
+            if (num % i === 0) return false;
         }
-    })
-    return answer;
-
-    function isPrime(number) {
-        if(number === 1){
-            return false;
-        }
-        if((number > 2) && (number%2 === 0)){
-            return false;
-        }
-        for (let i = 3; i <= Math.sqrt(number); i+=2) {
-            if(number%i === 0){
-                return false;
-            }
-        }
+        
         return true;
     }
+
+    const baseNumber = n.toString(k);
+    const splitedBaseNumber = baseNumber.split('0');
+    return splitedBaseNumber.filter((numString) => numString && isPrimeNumber(numString)).length;
 }
