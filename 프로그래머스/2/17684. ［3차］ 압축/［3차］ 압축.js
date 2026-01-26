fac74305d7ebcb496574bb3af7f102cc9e510514
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-01-26 21:45:39 +0900

    [level 2] Title: [3차] 압축, Time: 11.80 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,25 +1,29 @@
 function solution(msg) {
-    const dictionary = new Map();
-    const defaultElement = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    defaultElement.split('').forEach((str,idx) => dictionary.set(str,idx+1));
-    const answer = [];
+    const dictionary = {
+      A: 1,  B: 2,  C: 3,  D: 4,  E: 5,  F: 6,
+      G: 7,  H: 8,  I: 9,  J: 10, K: 11, L: 12,
+      M: 13, N: 14, O: 15, P: 16, Q: 17, R: 18,
+      S: 19, T: 20, U: 21, V: 22, W: 23, X: 24,
+      Y: 25, Z: 26,
+    };
     
-    let first = 0;
-    let last = 1;
-    while(last <= msg.length) {
-        const target = msg.slice(first, last);
-        const nextTarget = msg.slice(first, last + 1);
-        
-        if(dictionary.has(nextTarget)){
-            last += 1;
-            continue;
+    const answer = [];
+    for (let i = 0; i < msg.length; i += 1) {
+        let target = msg[i];
+        let skip = 1;
+        while (i + skip < msg.length && dictionary[target + msg[i + skip]]) {
+            target += msg[i + skip];
+            skip += 1;
         }
-        answer.push(dictionary.get(target));
-        dictionary.set(nextTarget, dictionary.size + 1);
-        first = last;
-        last = first + 1;
+        
+        answer.push(dictionary[target]);
+        
+        if (i + skip < msg.length) {
+            dictionary[target + msg[i + skip]] = Object.keys(dictionary).length + 1
+        }
+        
+        i += target.length - 1;
     }
     
-    answer.push(dictionary.get(msg.slice(first, last-1)));
     return answer;
 }
