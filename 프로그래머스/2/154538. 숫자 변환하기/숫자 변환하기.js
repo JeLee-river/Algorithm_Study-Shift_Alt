8a34113458b8ec7a37d02781c01db5e7c16d626e
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-02-05 01:55:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 317.63 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,33 +1,28 @@
 function solution(x, y, n) {
-    const calculateList = ['n','2','3'];
-
-    function calculator(target, currentValue){
-        if (target === 'n') return currentValue + n;
-        if (target === '2') return currentValue * 2;
-        if (target === '3') return currentValue * 3;
-    }
-
-    const answerList = [];
-    const visited = new Set();
-    let queue = [[x, 0]];
-    visited.add(x);
-
-    while (queue.length > 0) {
-        const newQueue = [];
-        for(let i = 0; i <= queue.length - 1; i++){
-            const [currentValue, tryCount] = queue[i];
-            if(currentValue === y) return tryCount;
-            
-            calculateList.forEach((target) => {
-                const result = calculator(target, currentValue);
-
-                if (!visited.has(result) && result <= y) {
-                    visited.add(result);
-                    newQueue.push([result, tryCount + 1]);
-                }           
-            })
+    const multipleTwo = (num) => num * 2;
+    const multipleThree = (num) => num * 3;
+    const addN = (num) => num + n;
+    const calculator = [multipleTwo, multipleThree, addN];
+    
+    const visited = new Set([x]);
+    const answer = [];
+    const queue = [{current: x, count: 0}]
+    let currentIndex = 0;
+    while(queue.length > currentIndex) {
+        const {current, count} = queue[currentIndex];
+        if(current === y) {
+            return count;
         }
-        queue = [...newQueue];
+        
+        calculator.forEach((method) => {
+            const result = method(current);
+            if(result <= y && !visited.has(result)){
+                visited.add(result);
+                queue.push({current: result, count: count + 1});   
+            }
+        });
+        currentIndex += 1;
     }
+    
     return -1;
 }
