bff676206a41ce0986d3e29c10aca93f982506aa
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-04-11 02:00:08 +0900

    [level 2] Title: 가장 큰 수, Time: 242.48 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,16 +1,6 @@
 function solution(numbers) {
-    const numbersString = numbers.map((number) => String(number));
-    const removeZero = (numberString) => numberString.replace(/0/g,'');
-    
-    numbersString.sort((a,b) => {
-        const totalString1 = a + b;
-        const totalString2 = b + a; 
-        return totalString2.localeCompare(totalString1);
-    });
-    
-    const answer = numbersString.join('');
-    if(removeZero(answer).length === 0){
-        return '0';
-    }
-    return answer;
+    const numString = numbers.map((num) => String(num));
+    numString.sort((prev, next) => (next + prev).localeCompare(prev + next));
+    const answer = numString.join('');
+    return answer[0] === '0' ? '0' : answer;
 }
